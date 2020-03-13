dd6aeb42b34c95c50a958419e832f273175194da
Peizhi Liu <peizhiliu168@gmail.com>
2020-03-12 23:42:34 -0500

    board tests

--- test/board_test.cxx
+++ test/board_test.cxx
@@ -1,7 +1,26 @@
 #include "board.hxx"
 #include <catch.hxx>
+#include <math.h>
+#include "geometry.hxx"
 
-TEST_CASE("create virtual board")
+TEST_CASE("create and reset virtual board")
 {
-    Board b_board{5,5};
+    Geometry geometry{};
+
+    Board b_board{geometry.board_size.width, geometry.board_size.height};
+    std::vector<ge211::Rectangle> squares = b_board.get_squares();
+    std::vector<ge211::Rectangle> walls = b_board.get_walls();
+
+    CHECK(squares.size() == pow(geometry.board_size.width, 2));
+    CHECK((geometry.board_size.width - 1) *
+          (geometry.board_size.height - 1) == walls.size());
+
+    b_board.reset();
+    walls = b_board.get_walls();
+    squares = b_board.get_squares();
+
+    CHECK(squares.size() == pow(geometry.board_size.width, 2));
+    CHECK((geometry.board_size.width - 1) *
+          (geometry.board_size.height - 1) == walls.size());
+
 }
