e751282cf320ab4f9d205d42258efb89c56127ad
Peizhi Liu <peizhiliu168@gmail.com>
2020-03-13 16:40:10 -0500

    board tests

--- test/board_test.cxx
+++ test/board_test.cxx
@@ -24,3 +24,24 @@ TEST_CASE("create and reset virtual board")
           (geometry.board_size.height - 1) == walls.size());
 
 }
+
+TEST_CASE("board within screen dim")
+{
+    Geometry geometry{};
+
+    Board b_board{geometry.board_size.width, geometry.board_size.height};
+
+    std::vector<ge211::Rectangle> walls = b_board.get_walls();
+    for (ge211::Rectangle  wall : walls){
+        CHECK(wall.top_left().x >= 0);
+        CHECK(wall.top_left().x <= geometry.scene_dims.width);
+        CHECK(wall.top_left().y >= 0);
+        CHECK(wall.top_left().y <= geometry.scene_dims.width);
+
+        CHECK(wall.bottom_right().x >= 0);
+        CHECK(wall.bottom_right().x <= geometry.scene_dims.width);
+        CHECK(wall.bottom_right().y >= 0);
+        CHECK(wall.bottom_right().y <= geometry.scene_dims.width);
+
+    }
+}
